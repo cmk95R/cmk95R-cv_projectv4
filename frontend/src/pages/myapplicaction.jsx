143d5bad79b0193613e982aac197270c04de70d1
Cristian Velazquez <109715719+cmk95R@users.noreply.github.com>
2025-10-23 02:58:12 -0300

    fix import

--- frontend/src/pages/myapplicaction.jsx
+++ frontend/src/pages/myapplicaction.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState, useCallback } from "react";
+import React, { useEffect, useContext,useMemo, useState, useCallback } from "react";
 import {
   Box,
   Typography,
